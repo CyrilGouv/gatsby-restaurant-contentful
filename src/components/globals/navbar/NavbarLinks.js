5f5684a919e4a77972b258c8123f21defa987342
Cyril Gouverneur <cyril@imac-de-cyril.home>
2020-07-17 23:08:26 +0200

    Add Navbar links CSS

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -7,25 +7,48 @@ import { styles } from '../../utils/index'
 const NavbarLinks = ({ isOpen, toggleMenu }) => {
     
     return (
-        <LinkWrapper open={ isOpen }>
-            <ul>
-                {
-                    menuLinks.map(link => {
-                        return (
-                            <li key={ link.id }>
-                                <Link className="nav-link" to={ link.path }>{ link.name }</Link>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+        <LinkWrapper className={ isOpen ? 'isOpen' : '' }>
+            {
+                menuLinks.map(link => {
+                    return (
+                        <li key={ link.id }>
+                            <Link className="nav-link" to={ link.path }>{ link.name }</Link>
+                        </li>
+                    )
+                })
+            }
         </LinkWrapper>
     )
     
 }
 
-const LinkWrapper = styled.div`
+const LinkWrapper = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    height: 0;
+    overflow: hidden;
+    ${styles.transDefault};
 
+    &.isOpen {
+        height: 152px;
+    }
+
+    .nav-link {
+        display: block;
+        text-decoration: none;
+        padding: .5rem 1rem .5rem 1rem;
+        color: ${styles.colors.mainGrey};
+        text-transform: capitalize;
+        font-weight: 700;
+        ${styles.transDefault};
+
+        &:hover {
+            background: ${styles.colors.mainGrey};
+            color: ${styles.colors.mainYellow};
+            padding: .5rem 1rem .5rem 1.3rem;
+        }
+    }
 `
 
 export default NavbarLinks
