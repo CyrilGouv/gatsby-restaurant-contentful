32d58489f81b8943ab4a50da64c8fb55d8e7d23c
Cyril Gouverneur <cyril@imac-de-cyril.home>
2020-07-17 22:47:30 +0200

    Add Menu Links to constants folder

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import menuLinks from '../../../constants/menuLinks'
+import { styles } from '../../utils/index'
 
 const NavbarLinks = ({ isOpen, toggleMenu }) => {
     
     return (
-        <div>
-            COntact from navbar links
-        </div>
+        <LinkWrapper open={ isOpen }>
+            <ul>
+                {
+                    menuLinks.map(link => {
+                        return (
+                            <li key={ link.id }>
+                                <Link className="nav-link" to={ link.path }>{ link.name }</Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </LinkWrapper>
     )
     
 }
+
+const LinkWrapper = styled.div`
+
+`
 
 export default NavbarLinks
---
+++ src/constants/menuLinks.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 0,
+        path: '/',
+        name: 'home'
+    }, 
+    {
+        id: 1,
+        path: '/about',
+        name: 'about'
+    },
+    {
+        id: 2,
+        path: '/blog',
+        name: 'blog'
+    },
+    {
+        id: 3,
+        path: '/contact',
+        name: 'contact'
+    }
+]
