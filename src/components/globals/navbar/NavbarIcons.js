a210a10f80200c19846fcc80456e42aaf924624f
Cyril Gouverneur <cyril@imac-de-cyril.home>
2020-07-18 10:33:11 +0200

    Add Navbar Icons & constants

--- src/components/globals/navbar/NavbarIcons.js
+++ src/components/globals/navbar/NavbarIcons.js
@@ -1,11 +1,55 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
+import { styles } from '../../utils/index'
+import socialIcons from '../../../constants/socialIcons'
 
-export default class NavbarIcons extends Component {
-    render() {
-        return (
-            <div>
-                content from icons
-            </div>
-        )
-    }
+const NavbarIcons = () => {
+   
+    return (
+        <IconWrapper>
+            {
+                socialIcons.map((social, index) => {
+                    return (
+                        <a className="icon" target="_blank" rel="noopener noreferrer" key={ index } href={ social.url }>{ social.icon }</a>
+                    )
+                })
+            }
+        </IconWrapper>
+    )
+    
 }
+
+const IconWrapper = styled.div`
+    display: none;
+
+    .icon {
+        font-size: 1.3rem;
+        ${styles.transDefault};
+
+        &:nth-of-type(1) {
+            color: #3b579d;
+        }
+
+        &:nth-of-type(2) {
+            color: #3ab7f0;
+        }
+
+        &:nth-of-type(3) {
+            color: #da5f53;
+        }
+
+        &:hover {
+            color: ${styles.colors.mainYellow};
+        }
+
+    }
+
+    @media (min-width: 768px) {
+        display: flex;
+        width: 10rem;
+        justify-content: space-around;
+    }
+
+`
+
+export default NavbarIcons
